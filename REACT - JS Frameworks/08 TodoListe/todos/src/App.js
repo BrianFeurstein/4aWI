8d63a4e4c1288a0d74abc72b3f9367073eee7cba
BrianFeurstein <79904813+BrianFeurstein@users.noreply.github.com>
2022-02-22 20:55:53 +0100

    todoliste ist fertig

---
+++ REACT - JS Frameworks/08 TodoListe/todos/src/App.js
@@ -0,0 +1,58 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import TaskAdder from './components/TaskAdder';
+import TodoList from './components/Todolist';
+import { Component } from 'react';
+
+
+class App extends Component{
+
+constructor(props){
+  super(props);
+  this.state = {
+    todos:[{
+      "id":1,
+      "name":"einkaufen",
+      "done":false
+  },
+  {
+    "id":2,
+    "name":"putzen",
+    "done":false
+},
+
+
+]
+  }
+}
+
+
+addTask = (value)=>{
+  let todo = {
+    id:1,
+    name:value,
+    done:false
+  }
+  let todos = this.state.todos;
+  todos.push(todo);
+
+  this.setState({
+    todos: todos
+  })
+  
+}
+
+  render(){
+    return (
+      <div className="App">
+        <Header/>
+        <TaskAdder onTaskAdded={this.addTask}/>
+        <TodoList todos={this.state.todos}/>
+      </div>
+    );
+  }
+}
+
+
+export default App;
---
+++ REACT - JS Frameworks/08 TodoListe/todos/src/components/TaskAdder.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+
+class TaskAdder extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            inputValue:""
+        }
+    }
+    addTask = ()=>{
+        this.props.onTaskAdded(this.state.inputValue)
+        
+    }
+    handleChange=(event)=>{
+        this.setState({
+            inputValue:event.target.value
+        })
+    }
+
+    render(){
+        return(
+            <div>
+               <input type="text" value={this.state.inputValue} onChange={this.handleChange}/>
+               <button onClick={this.addTask}>Save</button>
+            </div>
+        )
+    }
+}
+
+export default TaskAdder;
